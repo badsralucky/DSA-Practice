bfe62c75b899a63c237274951f6a51528e60f84d
badsralucky <92315225+badsralucky@users.noreply.github.com>
2026-09-17 16:45:32 +0530

    Time: 8 ms (15.43%) | Memory: 262.9 MB (53.77%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n  = nums.size();
+        k  = (k % n);
+        if( k == 0) return;
+        vector<int> temp ;
+        // storing the values to temp 
+        for(int i = n-k ; i<n ; i++){
+            temp.push_back(nums[i]);
+        }
+        // shifting the valuse
+        for(int i =n-k-1; i>=0; i--){
+            nums[i+k]= nums[i];
+        }
+        for(int i = 0;i<k ;i++){
+            nums[i] = temp[i];
+        }
+    }
+};
